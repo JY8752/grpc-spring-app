c0ca0f36f218b96c07fa720d2296854c21cdc380
JY8752 <j.y.8752@gmail.com>
2022-03-11 00:08:48 +0900

    first commit

---
+++ src/main/kotlin/com/example/grpcspringapp/GrpcSpringAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.grpcspringapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrpcSpringAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrpcSpringAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/grpcspringapp/GrpcSpringAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.grpcspringapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrpcSpringAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
